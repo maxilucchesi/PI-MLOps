dbf4441026700eee2c28ea676d0e29e7c3dd0812
maxilucchesi <maximilianolucchesi@gmail.com>
2024-05-01 16:48:43 -0300

    trabajando en el main

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, world!"}
+from typing import Union, List
+import os
+import string
+import re
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from dateutil import parser
+import pandas as pd
+import pyarrow.parquet as pq
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+import dask.dataframe as dd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
