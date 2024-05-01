8ea1e4a4879bb218385181a21e4fa1c02b3300af
maxilucchesi <maximilianolucchesi@gmail.com>
2024-05-01 20:13:29 -0300

    agregue el sistema de recomendacion y la funcion sentiment analysis

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ def read_root():
     welcome_message = 'Proyecto Individual - MLOps - maxilucchesi'
     return welcome_message
 
+
+
+# Función UsersWorstDeveloper
+
 @app.get("/worst_developers/{year}")
 def get_worst_developers(year: int):
     """
@@ -37,7 +41,6 @@ def get_worst_developers(year: int):
     """
     return UsersWorstDeveloper(year)
 
-# Función UsersWorstDeveloper
 def UsersWorstDeveloper(year: int):
     """
     Devuelve el top 3 de desarrolladoras con juegos MENOS recomendados por usuarios para el año dado.
@@ -68,3 +71,81 @@ def UsersWorstDeveloper(year: int):
     result = [{"Top 3{}".format(i + 1): developer} for i, (developer, _) in enumerate(peores_devs.items())]
 
     return result
+
+
+
+
+# Función sentiment_analysis
+
+@app.get("/sentiment-analysis/{dev}")
+def sentiment_analysis(dev: str):
+
+    # Lee el archivo parquet y obtiene la ruta del directorio actual del script
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    path_to_parquet = os.path.join(current_directory, 'data', 'id_dev_sentiment.parquet')
+    id_dev_sentiment = pq.read_table(path_to_parquet).to_pandas()
+    
+
+    # Filtra el dataframe para el desarrollador dado
+    df_filtered = id_dev_sentiment[id_dev_sentiment['developer'] == dev]
+
+    if df_filtered.empty:
+        raise HTTPException(status_code=404, detail=f"No se encontraron registros para el desarrollador {dev}")
+
+    # Agrupa por análisis de sentimiento y cuenta la cantidad de registros
+    analysis_counts = df_filtered.groupby('sentiment_analysis').size().to_dict()
+
+    # Construye el resultado como un diccionario con una lista
+    result = {dev: [f'Negative = {analysis_counts.get(0, 0)}', f'Positive = {analysis_counts.get(2, 0)}']}
+
+    return result
+
+
+
+
+
+
+
+# Modelo de Recomendación de juegos, ingresando su id
+
+@app.get("/recomendacion_juego/{id_juego}")
+def recomenacion_juego(id_juego: int):
+
+    # Lee el archivo parquet y obtiene la ruta del directorio actual del script
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    path_to_parquet = os.path.join(current_directory, 'data', 'df_games_filtered.parquet')
+    df_games_filtered = pq.read_table(path_to_parquet).to_pandas()
+    df = df_games_filtered
+
+    # Se instancia un objeto vectorizador usando TfidfVectorizer, con stop words en inglés.
+    vectorizador = TfidfVectorizer(stop_words='english')
+
+    # Los valores nulos en la columna 'tags_concat' se reemplazan con una cadena vacía.
+    df_games_filtered['tags_concat'] = df_games_filtered['tags_concat'].fillna('')
+
+    # Se realiza la transformación TF-IDF en los datos de la columna 'tags_concat'.
+    vector_matrix = vectorizador.fit_transform(df_games_filtered['tags_concat'])
+
+    # Se calcula la similitud del coseno
+    simil_coseno = linear_kernel(vector_matrix, vector_matrix)
+
+    # Se crea una serie de indices
+    index = pd.Series(df_games_filtered.index, index=df_games_filtered['id']).drop_duplicates()
+
+    try:
+        # Buscar el nombre del juego correspondiente al ID
+        nombre_juego = df_games_filtered.loc[df_games_filtered['id'] == id, 'app_name'].iloc[0]
+
+        indice = index[id]
+        similitud = list(enumerate(simil_coseno[indice]))
+        similitud = sorted(similitud, key=lambda x: x[1], reverse=True)
+        idx_game_recommended = [i[0] for i in similitud[1:6]]
+        resultado_recomendacion = df_games_filtered['app_name'].iloc[idx_game_recommended]
+
+        print('Si te gustó:', nombre_juego, '\n')
+        print('Te recomendamos:')
+        for juego_recomendado in resultado_recomendacion:
+            print('-', juego_recomendado)
+
+    except KeyError:
+        print(id, 'no se encuentra en nuestra base.')
